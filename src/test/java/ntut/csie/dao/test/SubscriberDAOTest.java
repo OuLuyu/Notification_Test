291b9cd2843ed6ddef760b3a154cd725ec8f8819
HsinHsieh <sam786890@gmail.com>
2017-04-25 11:59:36 +0800

    try juint

---
+++ src/test/java/ntut/csie/dao/test/SubscriberDAOTest.java
@@ -0,0 +1,25 @@
+package ntut.csie.dao.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SubscriberDAOTest {
+	
+	@Before	
+	public void setup(){
+		
+	}
+	
+	@After
+	public void teardown(){
+		
+	}
+	
+	@Test
+	public void test(){
+		
+	}
+}
